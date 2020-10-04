c694f3882cdab14aa33dcab62c098a25e23062f6
Matt Del Signore <mattdelsig@me.com>
2020-10-04 19:41:16 -0400

    Remove buefy

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,5 @@
 import { createApp } from "vue";
-import Buefy from "buefy";
 import App from "./App.vue";
-import "./assets/scss/app.scss";
 
-Vue.use(Buefy);
-
-createApp(App).mount("#app");
+const app = createApp(App);
+app.mount("#app");
